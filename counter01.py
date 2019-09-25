0cddc4a8e9a29a016a3c7b2613111c7f059ed099
furusawa057 <sfurusawa057@gmail.com>
2019-09-25 14:08:32 +0900

    classの練習
    self.valueとvalueは違うもの？
    →値の名前が違っても機能することから確認できた

--- counter01.py
+++ counter01.py
@@ -1,13 +1,13 @@
 class MyCounterV1:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, val):
+        self.value = val
 
     def count_up(self):
         self.value += 1
 
 
 def main():
-    counter1 = MyCounterV1(value=0)
+    counter1 = MyCounterV1(val=0)
     print(counter1.value)
 
     counter1.count_up()
@@ -16,7 +16,7 @@ def main():
     counter1.count_up()
     print(counter1.value)
 
-    counter2 = MyCounterV1(value=7)
+    counter2 = MyCounterV1(val=7)
     print(counter2.value)
 
     counter2.count_up()
